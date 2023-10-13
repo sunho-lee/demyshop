d2039ec00b5889347e355c4c4c397c5df4f3a192
sunho-lee <sh112123@naver.com>
2023-10-13 15:24:09 +0900

    project init

---
+++ src/main/java/com/example/demyshop/DemyshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemyshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemyshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demyshop/DemyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemyshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
